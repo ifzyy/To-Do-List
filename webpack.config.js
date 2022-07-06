dd9a30e78422560064d67001bdf04d982c2eedc8
ifzyy <johnsonnifemi8@gmail.com>
2022-07-07 00:01:08 +0100

    fixed linters

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'To-do list',
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   output: {
