9280a086d284f91b2ddb512ef2bb532164c1621b
ifzyy <johnsonnifemi8@gmail.com>
2022-07-06 23:59:53 +0100

    fixed linters

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-import "./style.css";
+import './style.css';
 
 const items = [
   {
-    description: "Wash the dishes",
+    description: 'Wash the dishes',
     completed: false,
     index: 1,
   },
   {
-    description: "Complete To Do List project",
+    description: 'Complete To Do List project',
     completed: false,
     index: 2,
   },
   {
-    description: "Watch Documentary ",
+    description: 'Watch Documentary ',
     completed: false,
     index: 3,
   },
   {
-    description: "Family meeting",
+    description: 'Family meeting',
     completed: false,
     index: 4,
   },
 ];
 
 const createList = () => {
-  const listContainer = document.querySelector(".todo-container");
-  const unorderedList = document.createElement("ul");
-  unorderedList.classList.add("listItems");
+  const listContainer = document.querySelector('.todo-container');
+  const unorderedList = document.createElement('ul');
+  unorderedList.classList.add('listItems');
   listContainer.appendChild(unorderedList);
 
   items.forEach((task) => {
-    const list = document.createElement("li");
-    list.classList.add("list");
+    const list = document.createElement('li');
+    list.classList.add('list');
     list.innerHTML = `
     <div class='task-container'>
       <div class='task'>
@@ -44,3 +44,4 @@ const createList = () => {
   });
 };
 createList();
+
